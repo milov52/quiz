35e361e8cb9f7ad8c4479b8d56f038758e6fd39a
killerarz <milov52@gmail.com>
2023-01-03 20:21:06 +0300

    created echo bot

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-import re
-from pprint import pprint
+import os
 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+from dotenv import load_dotenv
 
-def main():
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+def echo(bot, update):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+def divide_question_file():
     with open('questions/1vs1200.txt', 'r', encoding='KOI8-R') as questions_file:
         file_data = questions_file.read().split('\n\n')
 
@@ -17,9 +30,17 @@ def main():
             data = data.replace('\n', ' ')
             answers.append(data[data.find(':')+2:])
 
-    print(questions)
-    print(answers)
+def main():
+    load_dotenv()
+    # divide_question_file()
 
+    token = os.getenv('TOKEN')
+    updater = Updater(token)
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_error_handler(error)
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
     main()
