f6e0e006ffc96b9b9aaf437e9a4f49cb02b24481
killerarz <milov52@gmail.com>
2023-01-03 20:01:01 +0300

    divided questions and answers

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import re
+from pprint import pprint
+
 
 def main():
     with open('questions/1vs1200.txt', 'r', encoding='KOI8-R') as questions_file:
-        questions = questions_file.read()
+        file_data = questions_file.read().split('\n\n')
+
+    questions = []
+    answers = []
+    for data in file_data:
+        if data.startswith('Вопрос') or data.startswith('\nВопрос'):
+            data = data.replace('\n', ' ')
+            questions.append(data[data.find(':')+2:])
+
+        if data.startswith('Ответ') or data.startswith('\nОтвет'):
+            data = data.replace('\n', ' ')
+            answers.append(data[data.find(':')+2:])
 
     print(questions)
+    print(answers)
+
 
 if __name__ == '__main__':
     main()
